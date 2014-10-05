a0f2d3ee97d8fdbd4c6a11cfc0d7af02a7092306
Jason Frank <jason.frank@cadre5.com>
2014-10-05 17:46:20 -0400

    (1) Fixed a couple of overflow issues.  Now vertical scrolling is allowed and works fine when things like the security notice text is displayed and and the user must scroll down to see all of it. (2) Added "pointers" for the Aux Info Items (to point to their respective button). (3) Added a close button to each Aux Info Item.  I had an issue with overlaid divs stealing click events (since all of the those divs are absolutely positioned).  For now, one of the things that I tried which works (but has less-than-perfect side effects) is to make the non-selected div heights equal to 0.  Setting the height to 100% when "selected" did not "inflate" the div again (presumably b/c they are absolutely positioned), at least in terms of the background and border.  However, I actually like the look that resulted, with the border and triangle that resulted.  So for now I'm leaving it this way, but I need to figure out how to add a background underneath the content of the selected div.

--- LoginPrototype.js
+++ LoginPrototype.js
@@ -4,6 +4,8 @@ $(document).ready(function (e) {
 
     var currentColorSchemeId = 1;
     var colorSchemes = ['Dark', 'Light', 'Blue'];
+    var $currentAuxInfoItem;
+    var $previousAuxInfoItem;
 
     //$(document).on("click", "#mainWrapper", function (e) {
     //    // --- Only handle clicks that were on the wrapper itself - not on child elements        
@@ -14,19 +16,39 @@ $(document).ready(function (e) {
     //    }
     //});
 
-
+    // Open Auxillary Info
     $(document).on('click', '#auxInfoButtons li', function (e) {
         var $selectedElement = $(e.target);
         // Get the parent LI in case a child element is the e.target. (for some reason child elements are also being targetted)
         var $selectedLI = $selectedElement.closest('li');
-        // Change button appearance
+        // --- Change button appearance
         $('#auxInfoButtons li').removeClass('Selected');
         $selectedLI.addClass('Selected');
-        // Reveal content
-        $('#auxInfo > .auxInfoItem').removeClass('Selected');
-        var newSelectedContentId = '#' + $selectedLI.data('auxInfoId');
-        $(newSelectedContentId).addClass('Selected');
+        // --- Content: Hide previous one and reveal new one
+        // Make the current on the previous one
+        $previousAuxInfoItem = $currentAuxInfoItem;
 
+        // Make the new one the "current" one
+        var newSelectedContentId = '#' + $selectedLI.data('auxInfoId');
+        $currentAuxInfoItem = $(newSelectedContentId);
+
+        if ($previousAuxInfoItem) {
+            $previousAuxInfoItem.removeClass('Selected');
+            // Need to set display none on the previous one after its opacity fades so that 
+            //  mouse events can pass through to other aux info items
+//            setTimeout(function () { $previousAuxInfoItem.css('display', 'none'); }, 500);
+        }
+//        $currentAuxInfoItem.css('display', 'block');
+        $currentAuxInfoItem.addClass('Selected');
+    });
+
+    // Close Aux Info
+    $(document).on('click', '.auxInfoItem .CloseAuxInfoButton', function (e) {
+        // Change button appearance
+        $('#auxInfoButtons li').removeClass('Selected');
+        // Hide content
+        $('#auxInfo .auxInfoItem').removeClass('Selected');
+        $currentAuxInfoItem = $previousAuxInfoItem = null;
     });
 
     // Color scheme switching
@@ -52,6 +74,7 @@ $(document).ready(function (e) {
         $('.MenuButton').toggleClass('MenuShown');
     });
 
+    // Enlarge/Shrink the logo (demonstrate SVG scaling)
     $(document).on("click", ".Logo", function (e) {
         
         $('.Logo').toggleClass('LogoSizeBig');
